f5d93768a1d55be4ea53ba70ad346178b3d7a5c8
NatthasaKendo <kendokeyword@gmail.com>
2022-10-11 10:48:27 +0700

    add cors

--- back/server.ts
+++ back/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import routes from './routes';
 import bodyParser from 'body-parser';
 import { connectDB } from './config/db';
+import cors from 'cors';
 
 dotenv.config({
     path: "./config/config.env"
@@ -13,6 +14,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 }); 
